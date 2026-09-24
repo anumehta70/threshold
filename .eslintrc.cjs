c8229b38bd537956a6cec098b1e226293e5252b2
anumehta70 <anumehta70@users.noreply.github.com>
2026-09-24 15:05:01 +0530

    Fix linting configuration and correct contract copy path in deployment workflow

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,7 +6,7 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs", "vite.config.ts", "managed"],
+  ignorePatterns: ["dist", ".eslintrc.cjs", "vite.config.ts", "managed", "preprod-deployment"],
   parser: "@typescript-eslint/parser",
   plugins: ["react-refresh"],
   rules: {
