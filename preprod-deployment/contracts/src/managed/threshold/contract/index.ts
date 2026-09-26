4ebc4c858574c016de705e37be0008fbc6a556c5
anumehta70 <anumehta70@users.noreply.github.com>
2026-09-26 11:13:17 +0530

    Suppress TS6205 for unused type parameters in Contract stub

--- preprod-deployment/contracts/src/managed/threshold/contract/index.ts
+++ preprod-deployment/contracts/src/managed/threshold/contract/index.ts
@@ -1,5 +1,7 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
+// eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
 export class Contract<T = any, U = any> {
+  _phantomT?: T;
+  _phantomU?: U;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   constructor(..._args: any[]) {}
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
